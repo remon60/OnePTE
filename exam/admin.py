5647595b6358eeae6207172834f336982962547d
remon60 <islammehedi60@gmail.com>
2024-08-20 21:42:03 +0600

    admin file simplified

--- exam/admin.py
+++ exam/admin.py
@@ -1,71 +1,48 @@
 from django.contrib import admin
-from .models import *  
+from .models import Question, Audio, Paragraph, Option, Submission
 
 
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'type') 
-    search_fields = ('type',)
-
-
-@admin.register(Audio)
-class AudioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'question_id', 'question_title', 'question_type', 'speaker', 'audio_file') 
-    search_fields = ('get_type',)
-
+class QuestionRelatedMixin:
     def question_id(self, obj):
         return obj.question.id
+    
     def question_title(self, obj):
         return obj.question.title
+    
     def question_type(self, obj):
         return obj.question.type
 
 
-@admin.register(Paragraph)
-class ParagraphAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_id', 'get_title', 'get_type', 'content') 
-    search_fields = ('get_type',)
 
-    def get_id(self, obj):
-        return obj.question.id
-    def get_title(self, obj):
-        return obj.question.title
-    def get_type(self, obj):
-        return obj.question.type
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'type')
+    search_fields = ('type',)
+
+
+
+@admin.register(Audio)
+class AudioAdmin(QuestionRelatedMixin, admin.ModelAdmin):
+    list_display = ('id', 'question_id', 'question_title', 'question_type', 'speaker', 'audio_file')
+    search_fields = ('question_type',)
+
+
+
+@admin.register(Paragraph)
+class ParagraphAdmin(QuestionRelatedMixin, admin.ModelAdmin):
+    list_display = ('id', 'question_id', 'question_title', 'question_type', 'content')
+    search_fields = ('question_type',)
+
 
 
 @admin.register(Option)
-class OptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_id', 'get_title', 'get_type', 'content', 'is_correct', 'correct_order') 
-    search_fields = ('get_type',)
+class OptionAdmin(QuestionRelatedMixin, admin.ModelAdmin):
+    list_display = ('id', 'question_id', 'question_title', 'question_type', 'content', 'is_correct', 'correct_order')
+    search_fields = ('question_type',)
 
-    def get_id(self, obj):
-        return obj.question.id
-    def get_title(self, obj):
-        return obj.question.title
-    def get_type(self, obj):
-        return obj.question.type
-    
+
 
 @admin.register(Submission)
-class SubmissionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'get_id', 'get_title', 'get_type', 'user_id', 'answer_text', 'ordered_paragraphs', 'selected_options', 'score') 
-    search_fields = ('get_type',)
-
-    def get_id(self, obj):
-        return obj.question.id
-    def get_title(self, obj):
-        return obj.question.title
-    def get_type(self, obj):
-        return obj.question.type
-    
-
-
-
-
-
-
-
-
-
-
+class SubmissionAdmin(QuestionRelatedMixin, admin.ModelAdmin):
+    list_display = ('id', 'question_id', 'question_title', 'question_type', 'user_id', 'answer_text', 'ordered_paragraphs', 'selected_options', 'score')
+    search_fields = ('question_type',)
